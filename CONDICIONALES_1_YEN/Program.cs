8888bb74bad80ae18679918eab57c9e767028f4b
hyenci21 <159926659+hyenci21@users.noreply.github.com>
2024-02-20 10:00:16 -0600

    AGREGUE PROGRAMA

---
+++ CONDICIONALES_1_YEN/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+
+
+public class Tarea
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("CALCULAR EL MAYOR DE 3 NUMEROS");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("");
+
+        Console.WriteLine("INGRESE EL PRIMER NUMERO A COMPARAR");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("INGRESE EL SEGUNDO NUMERO A COMPARAR");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("INGRESE EL TERCER NUMERO A COMPARAR");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine($"EL NUMERO MAYOR ES:   " + num1);
+        }
+        else if (num1 < num2)
+        {
+
+
+            Console.WriteLine($"EL NUMERO MAYOR ES:   " + num2);
+        }
+        else if (num1 == num2 && num2 == num3 && num1 == num3)
+        {
+
+
+            Console.WriteLine("Los numeros que usted ingreso son iguales, por favor ingrese otros valores");
+
+        }
+        else { Console.WriteLine($"EL NUMERO MAYOR ES:   " + num3); }
+
+        Console.WriteLine("----------------------------------------------------------");
+        Console.WriteLine("PROGRAMA 2");
+        Console.WriteLine("");
+        Console.WriteLine("INGRESE SU NOMBRE");
+        string nombre1 = Console.ReadLine();
+
+        Console.WriteLine("INGRESE SU EDAD");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        if (edad >= 18)
+        {
+            Console.WriteLine("BIENVENIDO AL CLUB MADS");
+
+        }
+        else
+        {
+            Console.WriteLine("USTED ES MENOR DE EDAD, NO PUEDE UNIRSE AL CLUB");
+        }
+
+
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Calcular el Precio Final de un Producto:");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("");
+        double descuento, des1;
+
+        Console.WriteLine("INGRESE EL PRECIO DEL PRODUCTO A COBRAR");
+        double precio = Convert.ToDouble(Console.ReadLine());
+        if (precio >= 100)
+        {
+            descuento = (precio * 10 / 100);
+            des1 = precio - descuento;
+            Console.WriteLine($"SU DESCUENTO FUE DE {descuento} y su TOTAL A PAGAR ES {des1}");
+        }
+        else
+        {
+            Console.WriteLine($"SU TOTAL A PAGAR ES: " + precio);
+        }
+
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Validar Usuario y Contraseña:");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("");
+        Console.WriteLine("INGRESE POR FAVOR SU NOMBRE DE USUARIO");
+        string usuario = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine("INGRESE SU CONTRASEÑA");
+        string contra = Console.ReadLine();
+        if (usuario == "ANA_21" && contra == "pandita12" || usuario == "Fer12h" && contra == "carliR")
+        {
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("!BIENVENIDO!");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("USUARIO Y CONTRASEÑA CORRECTOS");
+        }
+
+        else
+        {
+
+
+
+            Console.WriteLine("USUARIO O CONTRASEÑA INCORRECTOS");
+            Console.WriteLine("********************************");
+            Console.WriteLine("INTENTALO DE NUEVO");
+        }
+
+
+
+        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("Determinar si un Número es Par o Impar:");
+        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("");
+
+        Console.WriteLine("INGRESE UN NUMERO");
+        int numpI = Convert.ToInt32(Console.ReadLine());
+
+        if (numpI % 2 == 0)
+        {
+            Console.WriteLine("EL NUMERO INGRESADO ES PAR");
+            Console.WriteLine("");
+            Console.WriteLine(":)");
+
+
+
+        }
+        else
+        {
+            Console.WriteLine("EL NUMERO INGRESADO ES IMPAR");
+        }
+
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Mostrar un Mensaje de Aprobación o Rechazo de Préstamo:");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("");
+
+        Console.WriteLine("INGRESE SU EDAD");
+        int edad1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        Console.WriteLine("INGRESE EL MONTO DE SU PRESTAMO");
+        double prestamo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("");
+        if (prestamo <= 5000 && edad1 >= 60)
+        {
+            Console.WriteLine("**********************************");
+            Console.WriteLine("SU PRESTAMO FUE APROBADO CON EXITO");
+            Console.WriteLine("**********************************");
+        }
+        else
+        {
+
+            Console.WriteLine("***************************************");
+            Console.WriteLine("LO SENTIMOS, SU PRESTAMO FUE RECHAZADO");
+            Console.WriteLine("***************************************");
+        }
+
+        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("Calcular el Área de una Figura Geométrica:");
+        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("");
+        Console.WriteLine("POR FAVOR, INGRESE EL TIPO DE FIGURA GEOMÉTRICA (TRIÁNGULO, CUADRADO O CÍRCULO");
+        string figura = Console.ReadLine();
+
+        if (figura.ToLower() == "triángulo")
+        {
+            Console.WriteLine("INGRESE LA BASE DEL TRIANGULO");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("INGRESE LA ALTURA DEL TRIANGULO");
+            double alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+            Console.WriteLine($"EL AREA DEL TRIANGULO ES : {areaTriangulo}");
+        }
+        else if (figura.ToLower() == "cuadrado")
+        {
+            Console.WriteLine("INGRESE EL LADO DEL CUADRADO:");
+            double ladoCuadrado = Convert.ToDouble(Console.ReadLine());
+
+            double areaCuadrado = ladoCuadrado * ladoCuadrado;
+            Console.WriteLine($"EL AREA DEL CUADRADO: {areaCuadrado}");
+        }
+        else if (figura.ToLower() == "círculo")
+        {
+            Console.WriteLine("INGRESE EL RADIO DEL CIRCULO:");
+            double radioCirculo = Convert.ToDouble(Console.ReadLine());
+
+            double areaCirculo = Math.PI * Math.Pow(radioCirculo, 2);
+            Console.WriteLine($"EL AREA DEL CIRCULO: {areaCirculo}");
+        }
+        else
+        {
+            Console.WriteLine("FIGURA NO ENCONTRADA POR FAVOR VOLVER A INGRESAR LOS DATOS CORRECTAMENTE");
+        }
+
+    }//NO BORRAR
+}//NO BORRAR
--- SWITCH_EJERCICIO1/Program.cs
+++ SWITCH_EJERCICIO1/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 public class TAREA2
 {
     public static void Main(string[] args)
     {
 
-        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("---------------------------------------------------");
         Console.WriteLine("1.\tConvertir un Número de Letra a Número:");
-        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("---------------------------------------------------");
         Console.WriteLine("");
-        Console.WriteLine("INGRESE UN NUMERO DEL 1 AL 5 EN LETTRAS (UNO, DOS, TRES, CUATRO, CINCO");
+        Console.WriteLine("INGRESE UN NUMERO DEL 1 AL 5 EN LETRAS (UNO, DOS, TRES, CUATRO, CINCO");
         string numletra;
         int num = 0;
         numletra = Console.ReadLine().ToLower();
@@ -43,6 +44,11 @@ public class TAREA2
         Console.WriteLine($"EL NUMERO QUE USTED INGRESO ES:{num}");
         Console.WriteLine("**************************************");
 
+        Console.WriteLine("");
+        Console.WriteLine("");
+       
+
+
 
         Console.WriteLine("------------------------------------------------------");
         Console.WriteLine("2.\tMostrar el Día de la Semana a partir de un Número:");
@@ -55,25 +61,25 @@ public class TAREA2
         switch (dia)
         {
             case 1:
-                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES LUNES");
+                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES: >LUNES");
                 break;
             case 2:
-                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES MARTES");
+                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES:  >MARTES");
                 break;
             case 3:
-                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES MIERCOLES");
+                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES: >MIERCOLES");
                 break;
             case 4:
-                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES JUEVES");
+                Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES: >JUEVES");
                 break;
             case 5:
-             Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES VIERNES");
+             Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES: >VIERNES");
                 break;
             case 6:
-           Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES SABADO");
+           Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES: >SABADO");
                 break;
             case 7:
-            Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES DOMINGO");
+            Console.WriteLine("EL DIA DE LA SEMANA QUE USTED SELECCIONO ES: <DOMINGO");
                 break;
             default: Console.WriteLine("EL NUMERO DEL DIA QUE INGRESO ES INCORRECTO, VUELA A INTENTAR");
                 break;
@@ -81,13 +87,157 @@ public class TAREA2
 
         }
 
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
         Console.WriteLine("------------------------------------------------------");
         Console.WriteLine("3.\tCalcular el Importe a Pagar por un Servicio:");
         Console.WriteLine("------------------------------------------------------");
         Console.WriteLine("");
+        Console.WriteLine("INGRESE EL NUMERO DEL SERVICIO QUE DESEA HACERLE A SU AUTO");
+        Console.WriteLine("");
+        Console.WriteLine("1.Lavado de auto");
+        Console.WriteLine("2.Cambio de aceite");
+        Console.WriteLine("3.Revisión mecánica");
+
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        switch (num2)
+        {
+            case 1:
+                Console.WriteLine("*****************************");
+                Console.WriteLine("SU TOTAL A PAGAR ES DE Q35");
+                Console.WriteLine("*****************************");
+                break;
+            case 2:
+                Console.WriteLine("*****************************");
+                Console.WriteLine("SU TOTAL A PAGAR ES DE Q75");
+                Console.WriteLine("*****************************");
+                break;
+            case 3:
+                Console.WriteLine("*****************************");
+                Console.WriteLine("SU TOTAL A PAGAR ES DE Q80");
+                Console.WriteLine("*****************************");
+                break;
+        }
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine("4.\tMostrar un Mensaje de Bienvenida en Diferentes Idiomas:");
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine("");
+
+        Console.WriteLine("SELECIONE EL IDIOMA QUE PREFIERA");
+        Console.WriteLine("1.ESPAÑOL");
+        Console.WriteLine("2.INGLES");
+        Console.WriteLine("3.FRANCES");
+        int numi = Convert.ToInt32(Console.ReadLine());
+
+        switch (numi)
+        {
+            case 1:
+                Console.WriteLine("*********************");
+                Console.WriteLine("TEN UN LINDO DIA :)");
+                Console.WriteLine("*********************");
+                break;
+            case 2:
+                Console.WriteLine("*********************");
+                Console.WriteLine("HAVE A NICE DAY :)");
+                Console.WriteLine("*********************");
+                break;
+            case 3:
+                Console.WriteLine("*********************");
+                Console.WriteLine("TU ES INCROYABLE :)");
+
+                break;
+            default:
+                Console.WriteLine("LA OPCION QUE SELECCIONO ES INCORRECTA");
+                break;
+
+        }
+
+        Console.WriteLine("------------------------------------------------------");
+        Console.WriteLine("5.\tEvaluar la Calificación de un Examen:");
+        Console.WriteLine("------------------------------------------------------");
+        Console.WriteLine("");
+
+        Console.WriteLine("INGRESE LA NOTA DE EL PRIMER PARCIAL");
+        double parcial = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("INGRESE LA NOTA DE EL SEGUNDO PARCIAL");
+        double parcial2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("INGRESE LA NOTA DE ACTIVIDADES");
+        double acti1 = Convert.ToDouble(Console.ReadLine());
+        double zonami;
+        zonami = parcial + parcial2 + acti1;
+    
+
+        if (zonami >= 26)
+        {
+
+            Console.WriteLine("INGRESE LA NOTA DEl EXAMEN FINAL");
+            double exfinal = Convert.ToDouble(Console.ReadLine());
+            double nota;
+            nota = parcial + parcial2 + acti1 + exfinal;
+
+            switch (nota)
+            {
+                case > 101:
+                    Console.Write("ERROR EN LOS VALORES INGRESADOS");
+                  break;
+                case >= 90:
+                    Console.WriteLine("SU NOTA ES EXCELENTE, APROBADO");
+                    break;
+                case >= 80:
+                    Console.WriteLine("SU NOTA ES BUENA, APROBADO");
+                    break;
+                case >= 70:
+                    Console.WriteLine("SU NOTA ES SATISFACTORIA, APROBADO");
+                    break;
+                case <= 60:
+                    Console.WriteLine("SU NOTA ES INSATISFACTORIA, REPROBADO");
+                    Console.WriteLine("");
+                    Console.WriteLine("¿EL ESTUDIANTE SE SOMETIO A UN EXAMEN DE RECUPERACION?");
+                
+                    Console.WriteLine("SELECIONE EL NUMERO DE SU RESPUESTA 1.SI     2.NO");
+                    int respuesta = Convert.ToInt32(Console.ReadLine());
+                    if (respuesta == 1) {
+                        Console.WriteLine("INGRESE LA NOTA FINAL DEL EXAMEN DE RECUPERACION DEL ALUMNO");
+                        double recup = Convert.ToDouble(Console.ReadLine());
+                        if (recup >= 61)
+                        {
+                            Console.WriteLine("FELICIDADES APROBO EL CURSO EN SEGUNDA CONVOCATORIA");
+                            Console.WriteLine($"CON UNA NOTA DE : " + recup);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"EXAMEN DE RECUPERACIÓN REPROBADO CON UNA NOTA DE: " + recup);
+                        }
+
+                    }
+
+
+                    break;
+
+                default:
+                    Console.Write("VALORES FUERA DE RANGO DE SU NOTA");
+                    break;
+
+            }
 
 
 
+        }
+        else
+        {
+            Console.Write("NO LLEGO A ZONA MININA, HA SIDO REPROBADO, POR FAVOR REPETIR EL CURSO");
+        }
+
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
 
 
 
